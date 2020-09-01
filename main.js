6244a7578a200e1d4311536e5df3bba100a310b2
Alessandro <alessandro.boscato@gmail.com>
2020-09-01 17:17:20 +0200

    Add class() e removeClass()

--- main.js
+++ main.js
@@ -7,7 +7,7 @@
 // Consiglio del giorno:
 // Partite dal comprendere bene come è fatto il DOM e il CSS, potete usarli a nostro vantaggio!
 
-
+//SHOW E HIDE
 // $(".header-right > a").click(function() {
 //   $(".hamburger-menu").show();
 // });
@@ -16,6 +16,25 @@
 //   $(".hamburger-menu").hide();
 // });
 
+//ADD CLASS
+// l'errore che fai è che stai aggiungendo una classe che si chiama "hamburger-menu.active" quando in realtà devi aggiungere solo la classe "active"
+// Il puntino non serve perchè addClass funziona che dentro alle tonde devi solo mettere il nome della classe, il fatto che sia una classe lo si da per scontato
 $(".header-right > a").click(function() {
-  $("div.hamburger-menu").addClass(".hamburger-menu.active");
+  $("div.hamburger-menu").addClass("active");
 });
+
+$(".hamburger-menu .close").click(function() {
+  $(".hamburger-menu").removeClass("active");
+});
+
+// TENTATIVO VAR NON FUNZIONA
+// var visible = false;
+// $(".header-right > a").click(function() {
+//   if (visible == false) {
+//     $("div.hamburger-menu").addClass(".hamburger-menu.active");
+//     visible = true;
+//   } else {
+//     $("div.hamburger-menu").removeClass(".hamburger-menu.active");
+//     visible = false;
+//   };
+// });
